ac459e812c90840e4339887c74e0523f507b3f94
Artyom Chernykh <dev.chernykh@yandex.ru>
2024-03-04 17:29:46 +0300

    temp interests

---
+++ Migrations/20240304133742_UserInterest.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YourMatchTgBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserInterest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InterestUser");
+
+            migrationBuilder.CreateTable(
+                name: "StableUserInterest",
+                columns: table => new
+                {
+                    UserId = table.Column<long>(type: "INTEGER", nullable: false),
+                    InterestId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StableUserInterest", x => new { x.UserId, x.InterestId });
+                    table.ForeignKey(
+                        name: "FK_StableUserInterest_Interests_InterestId",
+                        column: x => x.InterestId,
+                        principalTable: "Interests",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StableUserInterest_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TemporaryUserInterest",
+                columns: table => new
+                {
+                    UserId = table.Column<long>(type: "INTEGER", nullable: false),
+                    InterestId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TemporaryUserInterest", x => new { x.UserId, x.InterestId });
+                    table.ForeignKey(
+                        name: "FK_TemporaryUserInterest_Interests_InterestId",
+                        column: x => x.InterestId,
+                        principalTable: "Interests",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TemporaryUserInterest_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TempUserPhoto",
+                columns: table => new
+                {
+                    UserId = table.Column<long>(type: "INTEGER", nullable: false),
+                    PhotoFileId = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TempUserPhoto", x => new { x.UserId, x.PhotoFileId });
+                    table.ForeignKey(
+                        name: "FK_TempUserPhoto_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StableUserInterest_InterestId",
+                table: "StableUserInterest",
+                column: "InterestId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TemporaryUserInterest_InterestId",
+                table: "TemporaryUserInterest",
+                column: "InterestId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StableUserInterest");
+
+            migrationBuilder.DropTable(
+                name: "TemporaryUserInterest");
+
+            migrationBuilder.DropTable(
+                name: "TempUserPhoto");
+
+            migrationBuilder.CreateTable(
+                name: "InterestUser",
+                columns: table => new
+                {
+                    InterestsId = table.Column<int>(type: "INTEGER", nullable: false),
+                    UsersId = table.Column<long>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InterestUser", x => new { x.InterestsId, x.UsersId });
+                    table.ForeignKey(
+                        name: "FK_InterestUser_Interests_InterestsId",
+                        column: x => x.InterestsId,
+                        principalTable: "Interests",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_InterestUser_Users_UsersId",
+                        column: x => x.UsersId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InterestUser_UsersId",
+                table: "InterestUser",
+                column: "UsersId");
+        }
+    }
+}
---
+++ Migrations/20240304142111_UserInterestChanges3.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YourMatchTgBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserInterestChanges3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TempInterestUser");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_TempInterest",
+                table: "TempInterest");
+
+            migrationBuilder.AddColumn<long>(
+                name: "UserId",
+                table: "TempInterest",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_TempInterest",
+                table: "TempInterest",
+                columns: new[] { "UserId", "InterestId" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TempInterest_InterestId",
+                table: "TempInterest",
+                column: "InterestId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TempInterest_Users_UserId",
+                table: "TempInterest",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TempInterest_Users_UserId",
+                table: "TempInterest");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_TempInterest",
+                table: "TempInterest");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TempInterest_InterestId",
+                table: "TempInterest");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "TempInterest");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_TempInterest",
+                table: "TempInterest",
+                column: "InterestId");
+
+            migrationBuilder.CreateTable(
+                name: "TempInterestUser",
+                columns: table => new
+                {
+                    TemporaryInterestsInterestId = table.Column<int>(type: "INTEGER", nullable: false),
+                    UsersId = table.Column<long>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TempInterestUser", x => new { x.TemporaryInterestsInterestId, x.UsersId });
+                    table.ForeignKey(
+                        name: "FK_TempInterestUser_TempInterest_TemporaryInterestsInterestId",
+                        column: x => x.TemporaryInterestsInterestId,
+                        principalTable: "TempInterest",
+                        principalColumn: "InterestId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TempInterestUser_Users_UsersId",
+                        column: x => x.UsersId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TempInterestUser_UsersId",
+                table: "TempInterestUser",
+                column: "UsersId");
+        }
+    }
+}
